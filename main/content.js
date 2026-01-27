54cccdfe2d301bae35d65f9cc714ffcd0c202f71
Boruto-code <hamburger_nb_666@outlook.com>
2026-01-27 17:11:00 +0800

    Update

--- main/content.js
+++ main/content.js
@@ -2,5 +2,9 @@ import { lib, game, ui, get, ai, _status } from "../../../noname.js";
 import characterSubstitutes from "../character/characterSubstitute.js";
 
 export function content(config, pack) {
-    lib.characterSubstitute = { ...characterSubstitutes };
+    for (let i in characterSubstitutes) {
+        lib.characterSubstitute[i] = characterSubstitutes[i];
+    }
+
+    // lib.characterSubstitute = { ...characterSubstitutes };
 }
